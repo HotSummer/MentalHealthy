50e98c1574c2b8fa5e2d0e2773e8b45d84ce219a
HotSummer <18020280132@163.com>
2015-08-19 00:07:08 +0800

    创建工程

---
+++ MentalHealthDoctor/MentalHealthDoctor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MentalHealthDoctor
+//
+//  Created by zbq on 15-8-19.
+//  Copyright (c) 2015年 zbq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
