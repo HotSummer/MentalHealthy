a00620987dd88478dbefb1f83a6a8dd5241d2b2d
HotSummer <jfsld1@jfslddeMacBook-Pro.local>
2015-10-09 16:10:47 +0800

    找医生
    找医生首页

---
+++ MentalHealthDoctor/MentalHealthDoctor/MHDModule/MHDFindDoc/MHDDocListController.m
@@ -0,0 +1,37 @@
+//
+//  MHDDocListController.m
+//  MentalHealthDoctor
+//
+//  Created by jfsld1989 on 15/10/9.
+//  Copyright (c) 2015年 zbq. All rights reserved.
+//
+
+#import "MHDDocListController.h"
+
+@interface MHDDocListController ()
+
+@end
+
+@implementation MHDDocListController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
